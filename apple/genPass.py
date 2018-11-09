fa5248c786072eb0d79e0d222eadc5965b1be804
daniel <drfavor@gmail.com>
2018-11-09 14:31:56 -0600

    working on code reusability for genPass.py

--- apple/genPass.py
+++ apple/genPass.py
@@ -4,25 +4,37 @@ import subprocess
 import random
 import string
 import hashlib
+
 i = 0
+length = input("length: ")
+hashAlg = raw_input("hashing algorithm: ")
 
 while i < 1000:
     randomArr = []
 
-    for x in range(0,8):
+    for x in range(0,length):
         randomArr.append(random.choice(string.ascii_lowercase))
 
-    rint = 0
-    rupper = 0
-    while rint == rupper:
-        rint = random.randint(0,7)
-        rupper = random.randint(0,7)
+    rInt = 0
+    rUpper = 0
+    rSymbol = 0
 
-    randomArr[rint] = str(random.randint(0,9))
-    randomArr[rupper] = random.choice(string.ascii_uppercase)
+    while rInt == rUpper:
+        rInt = random.randint(0,7)
+        rUpper = random.randint(0,7)
+        rSymbol = random.randint(0,7)
     
+    randomArr[rInt] = str(random.randint(0,9))
+    randomArr[rUpper] = random.choice(string.ascii_uppercase)
+    randomArr[rSymbol] = random.choice(string.punctuation)
     #  simply changing the "hashlib.md5" to another hash algo will allow it to gen other possibilities 
-    
-    command = "echo " + hashlib.sha256(str(''.join(randomArr))).hexdigest()
-    subprocess.Popen(command, shell=True)
+
+    if hashAlg == "SHA256":
+        #print(hashlib.sha256(str(''.join(randomArr))).hexdigest())
+        print(str(''.join(randomArr)))
+
+    if hashAlg == "MD5":
+        #print(hashlib.md5(str(''.join(randomArr))).hexdigest())
+        print(str(''.join(randomArr)))
+
     i += 1
