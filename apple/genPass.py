deb0f0cdbde14eb0b7e468131170ca82c76995f9
root <root@localhost.localdomain>
2019-04-02 00:10:36 -0500

    added salted SHA256 to apple

--- apple/genPass.py
+++ apple/genPass.py
@@ -4,10 +4,17 @@ import subprocess
 import random
 import string
 import hashlib
+import uuid
 
 i = 0
-length = input("length: ")
-hashAlg = raw_input("hashing algorithm: ")
+length = input()
+hashAlg = raw_input()
+
+def saltedSHA256(text):
+    salt = ''
+    for i in range(0,32):
+        salt += random.choice(''.join((string.ascii_letters + string.digits)))
+    return hashlib.sha256(salt + text).hexdigest() + ':' + salt
 
 while i < 1000:
     randomArr = []
@@ -20,21 +27,20 @@ while i < 1000:
     rSymbol = 0
 
     while rInt == rUpper:
-        rInt = random.randint(0,7)
-        rUpper = random.randint(0,7)
-        rSymbol = random.randint(0,7)
-    
+        rInt = random.randint(0,length-1)
+        rUpper = random.randint(0,length-1)
+        #rSymbol = random.randint(0,length-1)
+
     randomArr[rInt] = str(random.randint(0,9))
     randomArr[rUpper] = random.choice(string.ascii_uppercase)
-    randomArr[rSymbol] = random.choice(string.punctuation)
-    #  simply changing the "hashlib.md5" to another hash algo will allow it to gen other possibilities 
+    #randomArr[rSymbol] = random.choice(string.punctuation)
 
     if hashAlg == "SHA256":
-        #print(hashlib.sha256(str(''.join(randomArr))).hexdigest())
+        print(hashlib.sha256(str(''.join(randomArr))).hexdigest())
+    elif hashAlg == "MD5":
+        print(hashlib.md5(str(''.join(randomArr))).hexdigest())
+    elif hashAlg == "SHA256Salt":
+	print(saltedSHA256(str(''.join(randomArr))))
+    else:
         print(str(''.join(randomArr)))
-
-    if hashAlg == "MD5":
-        #print(hashlib.md5(str(''.join(randomArr))).hexdigest())
-        print(str(''.join(randomArr)))
-
     i += 1
